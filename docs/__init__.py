439b5822db36eb08aa62484683639fc75fde31fc
clement hauguel <clement.hauguel@sanofi.com>
2025-10-28 08:47:20 +0100

    Add docstring

--- docs/__init__.py
+++ docs/__init__.py
@@ -1,3 +1,16 @@
+
+"""
+Ce fichier __init__.py marque le dossier 'docs' comme un package Python.
+
+Il permet :
+- D'organiser les modules Python dans un espace de noms commun.
+- De définir les éléments accessibles lors d'un import global (`from docs import *`).
+- De centraliser les importations utiles à l'extérieur du package.
+
+En exposant certaines fonctions et constantes via __all__, ce fichier facilite
+l'importation contrôlée et propre des composants principaux du package.
+"""
+
 from .base01 import MA_CONSTANTE
 from .base01 import addition
 from .base01 import puissance
